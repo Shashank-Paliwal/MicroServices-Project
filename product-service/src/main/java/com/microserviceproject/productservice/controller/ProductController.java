e3157fd887770d55398964f61f3937b64d26528e
Shashank <paliwalshashank648@gmail.com>
2023-09-25 00:33:45 +0530

    First Commit, getting ahead with product services.

---
+++ product-service/src/main/java/com/microserviceproject/productservice/controller/ProductController.java
@@ -0,0 +1,19 @@
+package com.microserviceproject.productservice.controller;
+
+import com.microserviceproject.productservice.dto.ProductRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/product")
+public class ProductController {
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest){
+
+
+
+    }
+
+}
