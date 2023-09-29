f62ac1050715fc1513e868c03b7085c438d08a89
Shashank <paliwalshashank648@gmail.com>
2023-09-30 02:38:51 +0530

    Multiple ammendments. Created modules such as order service, inventory service, discovery server. Added dependencies in the root pom.xml file

---
+++ Microservices-Parent/order-service/src/main/java/com/microserviceproject/orderservice/dto/OrderRequest.java
@@ -0,0 +1,15 @@
+package com.microserviceproject.orderservice.dto;
+
+import com.microserviceproject.orderservice.model.OrderLineItems;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderRequest {
+    private List<OrderLineItemsDto> orderLineItemsDtoList;
+}
